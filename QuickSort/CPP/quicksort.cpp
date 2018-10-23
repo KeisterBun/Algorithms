84596526e53d78203ee1b7d6b6f5a0aee49dd826
Keith <keith.drew.13@gmail.com>
2018-10-23 05:37:47 -0400

    Updated to Hoare partition scheme

--- QuickSort/CPP/quicksort.cpp
+++ QuickSort/CPP/quicksort.cpp
@@ -9,7 +9,9 @@
 
 void swap(int *array, int index1, int index2);
 int partition(int *array, int low_index, int high_index);
+int hoare_partition(int *array, int low_index, int high_index);
 void quicksort(int *array, int low_index, int high_index);
+void hoare_quicksort(int *array, int low_index, int high_index);
 void print_array(int *array, int bound);
 
 int main() {
@@ -22,7 +24,7 @@ int main() {
 
     print_array(arr, ARRAY_SIZE);
     
-    quicksort(arr, 0, ARRAY_SIZE - 1);
+    hoare_quicksort(arr, 0, ARRAY_SIZE - 1);
 
     print_array(arr, ARRAY_SIZE);
 
@@ -53,12 +55,48 @@ int partition(int *array, int low_index, int high_index) {
     return return_index;
 }
 
+int hoare_partition(int *array, int low_index, int high_index) {
+    int pivot = array[low_index];
+    int i = low_index - 1;
+    int return_index = high_index + 1;
+
+    while (true) {
+        // Move index forward until low-side swap is ready
+        do {
+            i++;
+        } while (array[i] < pivot);
+
+        // Move index backward until high-side swap is ready
+        do {
+            return_index--;
+        } while (array[return_index] > pivot);
+
+        // Make sure the indices haven't crossed over each other
+        if (i >= return_index) {
+            return return_index;
+        }
+
+        // Perform the swap
+        swap(array, i, return_index);
+        print_array(array, ARRAY_SIZE);
+    }
+}
+
 void quicksort(int *array, int low_index, int high_index) {
     if (low_index < high_index) {
         int pivot_index = partition(array, low_index, high_index);
 
         quicksort(array, low_index, pivot_index - 1);
         quicksort(array, pivot_index + 1, high_index);
+    }
+}
+
+void hoare_quicksort(int *array, int low_index, int high_index) {
+    if (low_index < high_index) {
+        int pivot_index = hoare_partition(array, low_index, high_index);
+
+        hoare_quicksort(array, low_index, pivot_index);
+        hoare_quicksort(array, pivot_index + 1, high_index);
     }
 }
 
